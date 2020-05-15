bae19fe4d54bb393a44213654e12ffb4d7b4d103
Mauriani <maurianim@gmail.com>
2020-05-15 16:56:15 -0300

    Primeira - alterações

--- src/components/Tips/index.js
+++ src/components/Tips/index.js
@@ -1,12 +1,15 @@
 import React from 'react';
 import { View } from 'react-native';
 
-import { Container } from './styles';
+import { Container, Option, Title, Image} from './styles';
 
 export default function Tips(){
   return(
     <Container>
-      
+      <Option>
+        <Title>Pague as suas contas sem sair de casa</Title>
+        <Image />
+      </Option>
     </Container>
   )
 }
